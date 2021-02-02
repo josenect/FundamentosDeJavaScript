d50d7e3711bf4f17639222aea347c79f6fbe761b
Jose Rodriguez <josenect@hotmail.es>
2021-02-01 22:13:06 -0500

    funciones que retornar un valor return true o false utilizacion de const

--- condiciones.js
+++ condiciones.js
@@ -52,3 +52,22 @@ var persona2 ={
 
 
     imprimirProfesion(persona2)
+    imprimirSiEsMayorDeEdad(persona2)
+
+
+    const MAYORIA_DE_EDAD = 18 // se define uan constante para no dejar un numero en la validacion del metodo
+
+    function esMayorDeEdad(persona){   //se divide el metodo inicial en dos retorna un true o false 
+        return persona.edad>=MAYORIA_DE_EDAD  
+    }
+
+
+    function imprimirSiEsMayorDeEdad(persona){
+
+        if(esMayorDeEdad(persona)){    // si es true la respuesta del metodo esMayorDeEdad ingre a el if 
+             console.log(`${persona.nombre} es mayor de Edad`)
+        }else{
+            console.log(`${persona.nombre} no es mayor de edad`)
+        }
+
+    }
