10ebb2344e1be5f09bb1e3a40740742dd96aa7b2
Jose Rodriguez <josenect@hotmail.es>
2021-02-04 09:20:38 -0500

    efecto colaterar al utilizar arrow funcion en funciones de las clases

--- poo.js
+++ poo.js
@@ -13,4 +13,23 @@ var pablo = new Persona('Pablo', 'Andrés') // creacion de obcjetos
 var joaquin = new Persona('Joaquín', 'Perez')
 var rosa = new Persona('Rosa', 'Mosqueta')
 
+
+// las arrow function al definirse en un metodo de una clase generan un evento colateral dedido que al manipular las propiedades del prototipo 
+//con el this este sale del scope local debido a que el pertenece al scope global perdiendo la referencia de las propiedades del prototipo que estabamos haciendo referencia
+
+Persona.prototype.soyAlto = () => {
+    return this.altura > 1.8          //  el this se sube al scope global asiendo referencia a un atributo que no existe y lo define undefine
+}   
+
+//1.  definir los metodos mediantes funciones expresivas para no sufrir este efecto
+
+Persona.prototype.soyAlto = function () {
+    return this.altura > 1.8         
+}   
+//
+
+
 rosa.saludar()
+
+
+
