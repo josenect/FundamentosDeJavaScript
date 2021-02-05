7b8390376869ab2d5d3fad499b3e0a75d55f4a13
Jose Rodriguez <josenect@hotmail.es>
2021-02-04 23:19:41 -0500

    envio de funciones como parametros

--- poo.js
+++ poo.js
@@ -7,9 +7,14 @@ class Persona {
         this.apellido = apellido
     }
 
-    saludar(){  // metodos de la clase 
+    saludar(fn){  // metodos de la clase // se define que se recibe un funcion como parametro 
 
         console.log(`Hola me llamo ${this.nombre} ${this.apellido}`)
+
+        if (fn) {                                       // se pregunta si el metodo es real con true 
+            fn(this.nombre ,this.apellido,false)        // se ejecuta ese metodo y se enviar los parametros del mismo
+            
+        }
     }
 
     soyAlto(){   //metodos de la clase
@@ -30,16 +35,38 @@ class Desarollador extends Persona{   // clase hija palabra reservada extends se
 
     } 
     
-    saludar(){                             // se define el mismo nombre del metodo para cambair su funcionalidad internamente principio de polimorfismo
+    saludar(fn){                             // se define que si recibe un funcion como parametro 
         console.log(`me llamo ${this.nombre } y soy desarollador`)
 
+        if (fn) {                                       // se pregunta si el metodo es real con true 
+            fn(this.nombre ,this.apellido,true)        // se ejecuta ese metodo y se enviar los parametros del mismo el con el ultimo true para confirmar que es devo
+            
+        }
     }
 
 
 }
 
 
+ 
+function responderSaludo(nombre,apellido,esDev){    //funcion que se pasara como parametro 
+
+    console.log(`Buen dia ${nombre } ${apellido }`)   
+    if (esDev) {                                        // validad si es devo para responder con un mensaje personalizado 
+        console.log(`Ah no sabia que eras Desarollador`)
+        
+    }
+}
 
 
+// creacion de obcjetos  
+var pablo = new Persona('Pablo', 'Andrés',1.2) 
+var joaquin = new Persona('Joaquín', 'Perez',1.2)
+var rosa = new Desarollador('Rosa', 'Mosqueta',1.05)
+
+// llamado de metodos con y sin function como parametro 
+pablo.saludar()
+joaquin.saludar(responderSaludo)
+rosa.saludar(responderSaludo)
 
 
