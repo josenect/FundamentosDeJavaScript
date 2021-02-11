1cd638a783353a9ceb3334497c716ce123f69d29
Jose Rodriguez <josenect@hotmail.es>
2021-02-10 20:04:57 -0500

    Manejo de errores con callbacks

--- callback.js
+++ callback.js
@@ -16,29 +16,31 @@ const onPeopleResponse = function(persona){   // metodo que manipula la data que
 function obtenerPersonaje(id,callback){
   const url = `${API_URl}${PEOPLE_URL.replace(':id',id)}`  // ruta del recurso y id de elemento 
 
-  $.get(url,opts,function(persona){ //metodo de jquery para relizar la peticion se envia al callback hasta que tiene una respuesta
+  $.get(url,opts,callback).fail(function(){
 
-    persona=persona.result.properties        // solo tomo los datos de la persona
-    console.log(`Hola Soy ${persona.name}`)   // impresion de nombre de la persona
-
-if (callback) {
-  callback()
-}
-
-
-  })              
+    console.log(`sucedio un error al obtener el personaje ${id}` ) /// mensaje de ese callback solo se ejecuta si hay un error
+  })
+            
 
 }
-//Una manera de asegurar que se respete la secuencia en que hemos realizado múltiples tareas es utilizando callbacks,
-// con lo que se ejecutará luego, en cada llamada. Lo importante es que el llamado al callback se haga a través de una 
-//función anónima. Sin embargo, al hacerlo de esta manera generamos una situación poco deseada llamada CallbackHell.
+//Manejo de errores con callbacks
+//Para solucionar el problema de quedarnos sin conexión, u otro error similar, en medio de una sucesión de callbacks utilizamos el método fail().l.
 
 // se llaman los datos del siguiente personaje solo si ya llego las respuesta del primero mediante callback 
 
-obtenerPersonaje(1,function(){
-  obtenerPersonaje(2,function(){
-    obtenerPersonaje(3,function(){
-      obtenerPersonaje(4)
+obtenerPersonaje(1,function(persona){   // la function que recibe es un callback  que contiene anidados 5 llamados al metodo obtener 
+  persona=persona.result.properties        // solo tomo los datos de la persona
+  console.log(`Hola Soy ${persona.name}`)   // impresion de nombre de la persona
+  obtenerPersonaje(2,function(persona){
+    persona=persona.result.properties        // solo tomo los datos de la persona
+    console.log(`Hola Soy ${persona.name}`)   // impresion de nombre de la persona
+    obtenerPersonaje(3,function(persona){
+      persona=persona.result.properties        // solo tomo los datos de la persona
+      console.log(`Hola Soy ${persona.name}`)   // impresion de nombre de la persona
+      obtenerPersonaje(4, function(persona){
+        persona=persona.result.properties        // solo tomo los datos de la persona
+        console.log(`Hola Soy ${persona.name}`)   // impresion de nombre de la persona
+      })
     })
   })
 })
